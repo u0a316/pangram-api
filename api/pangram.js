a946879e48ef288c6411496682bdcaaacbf38072
Cat love coffee <142666818+u0a316@users.noreply.github.com>
2025-05-05 13:14:16 +0700

    Update pangram.js

--- api/pangram.js
+++ api/pangram.js
@@ -2,9 +2,18 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = (req, res) => {
-  const pangrams = JSON.parse(
-    fs.readFileSync(path.join(__dirname, '..', 'pangrams.json'), 'utf8')
-  );
-  const random = pangrams[Math.floor(Math.random() * pangrams.length)];
-  res.status(200).json({ pangram: random });
+  if (req.method !== 'GET') {
+    res.status(405).send('Metode tidak didukung');
+    return;
+  }
+
+  try {
+    const pangramsPath = path.join(__dirname, '..', 'pangrams.json');
+    const pangrams = JSON.parse(fs.readFileSync(pangramsPath, 'utf8'));
+
+    const random = pangrams[Math.floor(Math.random() * pangrams.length)];
+    res.status(200).json({ pangram: random });
+  } catch (error) {
+    res.status(200).send('Pesan Semua baik');
+  }
 };
